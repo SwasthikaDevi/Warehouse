19416ebdd81de89e34ef94658780d76d524fb2f6
Swasthika Devi <swasthimohan@gmail.com>
2017-06-21 11:06:54 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print(5+9)
+a=list()
+out=list()
+n=input("Enter the no .of element:")
+for i in range(n):
+    r=input("Enter an element:")
+    out.append(0)
+    a.append(r)
+for i in range(n):
+    if(a.count(a[i])>1):
+        if(out[a.index(a[i])]==0):
+            print(a[i])
+            for j in range(n):
+                if(a[j]== a[i]):
+                    out[j]=1
