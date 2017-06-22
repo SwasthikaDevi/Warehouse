8cb5b039a45350f4bf99b113045bd99817e042b2
Swasthika Devi <swasthimohan@gmail.com>
2017-06-22 16:58:54 +0530

    Update fibonacci.py

--- vowel/fibonacci.py
+++ vowel/fibonacci.py
@@ -2,9 +2,12 @@ n=input("Enter the no. of terms:")
 i=0
 a=-1
 b=1
-while i<n:
-    c=a+b
-    print (c)
-    a=b
-    b=c
-    i+=1
+if(n>0):
+    while i<n:
+        c=a+b
+        print (c)
+        a=b
+        b=c
+        i+=1
+else:
+    print("Give +ve no. of terms:")
