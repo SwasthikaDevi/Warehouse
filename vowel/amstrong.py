ceed9d850b56dbe5734b8df0d6f763ad0ab6852a
Swasthika Devi <swasthimohan@gmail.com>
2017-06-22 17:13:01 +0530

    Update amstrong.py

--- vowel/amstrong.py
+++ vowel/amstrong.py
@@ -1,10 +1,13 @@
 n=input("Enter a no.:")
-s=0
-q=n
-while(n>0):
-    p=n%10
-    s+=p*p*p
-    n=n/10
-print(s)
+if(n>0):
+    s=0
+    q=n
+    while(n>0):
+        p=n%10
+        s+=p*p*p
+        n=n/10
+    print(s)
 if(s==q):
-    print("It's amstrong no:")
+        print("It's amstrong no:")
+else:
+     print("The no. is not a amstrong no.")
