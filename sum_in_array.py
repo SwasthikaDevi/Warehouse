1d116d01ce94848fc26e364b64ab35be1c2a5f36
Swasthika Devi <swasthimohan@gmail.com>
2017-06-23 18:44:09 +0530

    Update sum_in_array.py

--- sum_in_array.py
+++ sum_in_array.py
@@ -1,5 +1,6 @@
 g=list()
 c=0
+
 n=input("Enter the no. of elements:")
 for i in range(n):
     e=input("Enter an element:")
@@ -8,9 +9,12 @@ s=input("Enter the sum element:")
 for i in range(n-1):
     for j in range(n):
         if g[i]+g[j]==s:
+            ans=g[i]
+            an2=g[j]
             c=1
             break
 if c==1:
     print("Yes,it is present")
+    print(ans,an2)
 else:
     print("No,its not there")
